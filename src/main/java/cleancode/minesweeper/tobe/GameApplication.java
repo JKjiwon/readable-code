00cfefbf1dbf570ed4addc31148d0b4adfb8f996
kimjiwon <jwkim.oa@gmail.com>
2025-03-08 22:49:33 +0900

    refactoring: DIP 적용

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -2,16 +2,31 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 // SRP: 프로그램의 진입점
 public class GameApplication {
 
     public static void main(String[] args) {
         GameLevel gameLevel = new Beginner();
-        Minesweeper minesweeper = new Minesweeper(gameLevel);
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+
         minesweeper.initialize();
         minesweeper.run();
     }
+
+    /**
+     * DIP (Dependency Inversion Principle)
+     *
+     * DI (Dependency Injection)
+     *
+     * IOC (Inversion of Control) : 프로그램의 제어권을 프레임워크에 넘긴다.
+     */
 
 }
 
--- src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
+++ src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
@@ -3,19 +3,26 @@ package cleancode.minesweeper.tobe;
 import cleancode.minesweeper.tobe.game.GameInitializable;
 import cleancode.minesweeper.tobe.game.GameRunnable;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
-import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
-import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class Minesweeper implements GameInitializable, GameRunnable {
 
     private final GameBoard gameBoard;
     private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
-    private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
-    private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
+
+    // Minesweeper 고수준 -> ConsoleInputHandler 고수준 :: 잘못됨
+    // Minesweeper 고수준 -> InputHandler 저수준 :: 변경
+    // 콘솔이 아니라 다른 방법으로 입출력을 받고 싶다면? InputHandler, OutputHandler 를 구현한 객체를 외부에서 주입하면된다.
+    // Minesweeper 입장에서는 input/output 방식을 고려하지 않아도 된다.
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
     private int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
 
-    public Minesweeper(GameLevel gameLevel) {
+    public Minesweeper(GameLevel gameLevel, InputHandler inputHandler, OutputHandler outputHandler) {
         this.gameBoard = new GameBoard(gameLevel);
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
     }
 
     @Override
@@ -25,17 +32,17 @@ public class Minesweeper implements GameInitializable, GameRunnable {
 
     @Override
     public void run() {
-        consoleOutputHandler.showGameStartComments();
+        outputHandler.showGameStartComments();
         while (true) {
             try {
-                consoleOutputHandler.showBoard(gameBoard);
+                outputHandler.showBoard(gameBoard);
 
                 if (doesUserWinTheGame()) {
-                    consoleOutputHandler.printGameWinningComment();
+                    outputHandler.showGameWinningComment();
                     break;
                 }
                 if (doesUserLoseTheGame()) {
-                    consoleOutputHandler.printGameLosingComment();
+                    outputHandler.showGameLosingComment();
                     break;
                 }
 
@@ -43,9 +50,9 @@ public class Minesweeper implements GameInitializable, GameRunnable {
                 String userActionInput = getUserActionInputFromUser();
                 actOnCell(cellInput, userActionInput);
             } catch (GameException e) {
-                consoleOutputHandler.printExceptionMessage(e);
+                outputHandler.showExceptionMessage(e);
             } catch (Exception e) {
-                consoleOutputHandler.printSimpleMessage("프로그램에 문제가 생겼습니다.");
+                outputHandler.showSimpleMessage("프로그램에 문제가 생겼습니다.");
             }
         }
     }
@@ -72,7 +79,7 @@ public class Minesweeper implements GameInitializable, GameRunnable {
             return;
         }
 
-        consoleOutputHandler.printSimpleMessage("잘못된 번호를 선택하셨습니다.");
+        outputHandler.showSimpleMessage("잘못된 번호를 선택하셨습니다.");
     }
 
     private void changeGameStatusToLose() {
@@ -88,13 +95,13 @@ public class Minesweeper implements GameInitializable, GameRunnable {
     }
 
     private String getUserActionInputFromUser() {
-        consoleOutputHandler.printCommentForUserAction();
-        return consoleInputHandler.getUserInput();
+        outputHandler.showCommentForUserAction();
+        return inputHandler.getUserInput();
     }
 
     private String getCellInputFromUser() {
-        consoleOutputHandler.printCommentForSelectingCell();
-        return consoleInputHandler.getUserInput();
+        outputHandler.showCommentForSelectingCell();
+        return inputHandler.getUserInput();
     }
 
     private boolean doesUserLoseTheGame() {
